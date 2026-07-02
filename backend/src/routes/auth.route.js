c855c354f8dfcfeeb059dbef800e0298c6c172ad
Bishal Barman <bikikaishyap@gmail.com>
2026-07-02 20:21:59 +0530

    Initial Commit

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get("/signup" , (req,res)=>{
+    res.send("Signup route is working");
+});
+
+router.get("/login" , (req,res)=>{
+    res.send("Login route is working");
+});
+
+router.get("/logout" , (req,res)=>{
+    res.send("Logout route is working");
+});
+
+export default router;
