cfe840cdeeb29361eb94509a3366d4f979ec82a9
Bishal Barman <bikikaishyap@gmail.com>
2026-07-03 23:31:24 +0530

    Rate limiting implemented

---
+++ backend/src/lib/arcjet.js
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/node";
+import { isSpoofedBot } from "@arcjet/inspect";
+import http from "node:http";
+
+const arcjetKey = process.env.ARCJET_KEY;
+if (!arcjetKey) {
+  throw new Error('Missing ARCJET_KEY environment variable');
+}
+
+const aj = arcjet({
+  key: arcjetKey, // Get your site key from https://app.arcjet.com
+  rules: [
+    // Shield protects your app from common attacks e.g. SQL injection
+    shield({ mode: "LIVE" }),
+    // Create a bot detection rule
+    detectBot({
+      mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+      // Block all bots except the following
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        // Uncomment to allow these other common bot categories
+        // See the full list at https://arcjet.com/bot-list
+        //"CATEGORY:MONITOR", // Uptime monitoring services
+        //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
+      ],
+    }),
+    // Create a token bucket rate limit. Other algorithms are supported.
+    slidingWindow({
+        mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+        max: 100,
+        interval: 60,
+    })
+  ],
+});
+
+export default aj;
